100b36d5f40ffd8b413f2e4fbe654be242e57371
Bibi <irin.m@kkumail.com>
2025-11-24 14:42:36 +0700

    delete kafka

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,6 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Connect to Kafka
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.KAFKA,
-    options: {
-      client: {
-        brokers: ['localhost:9092'],
-      },
-      consumer: {
-        groupId: 'nestjs-group',
-      },
-    },
-  });
-
   await app.listen(process.env.PORT ?? 3000);
-  console.log('App running on http://localhost:3000');
 }
 bootstrap();
